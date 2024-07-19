24212d9ab6cbcafcd60e1c05bec62564212d438c
Omer Lichter <omerlichter@gmail.com>
2024-07-20 00:49:04 +0300

    feat(package.json, index.html, app.config): config make for creating executable

--- apps/drones-client/src/app/app.config.ts
+++ apps/drones-client/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appRoutes } from './app.routes';
 import { AppService, AppServiceFactory } from './core/services/app.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimations(), { provide: AppService, useFactory: AppServiceFactory }],
+  providers: [
+    provideRouter(appRoutes, withHashLocation()),
+    provideAnimations(),
+    { provide: AppService, useFactory: AppServiceFactory },
+  ],
 };
